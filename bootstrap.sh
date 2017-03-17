be8687a7547fb994196df314d0cd04da26b06018
Derek Ditch <derek@rocknsm.io>
2017-03-17 19:23:57 +0000

    Initial stab at supporting a RHEL-based ISO

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,24 @@
 #!/bin/bash -eux
 
-yum -y install epel-release
+# RedHat Prep
+if grep "Red Hat Enterprise Linux Server" /etc/system-release; then
+  ## Get current repo configurations for optional and extras
+  eval $(cat enabled_repos.txt | awk '/rhel-7-server-optional/{o=1}; o && /Enabled:/ { opts_enabled=$2; o=0}; /rhel-7-server-extras/{e=1}; e && /Enabled:/ {extras_enabled=$2;e=0}; END { print "extras=" extras_enabled; print "optional=" opts_enabled }')
+  
+  if [ $optional -ne 1 ]; then
+   subscription-manager repos --enable=rhel-7-server-optional-rpms 
+  fi
+  
+  if [ $extras -ne 1 ]; then
+   subscription-manager repos --enable=rhel-7-server-extras-rpms 
+  fi
+else
+  yum -y install epel-release
+fi
+
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
 yum -y install python-pip python-jinja2 python-simplejson genisoimage pykickstart createrepo rsync isomd5sum syslinux pigz mock fuseiso libguestfs-tools-c
 
 pip install pythonpy
+
