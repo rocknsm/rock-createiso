cbc4deb3e77b621db715f529832267ab151660fd
admin <admin@simplerockbuild.simplerock.lan>
2018-08-13 18:11:45 +0000

    concourse working dir

--- concourse.sh
+++ concourse.sh
@@ -13,6 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# change working directory
+cd "$(dirname "$(realpath "$0")")";
+
 # Install dependencies
 . ./bootstrap.sh
 
