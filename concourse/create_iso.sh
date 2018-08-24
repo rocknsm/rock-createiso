c48378e3bad6b82854b6bf842021bf363f0b0b0d
Johnathon Hall <spartan782@gmail.com>
2018-08-24 12:05:17 -0600

    Hide password output

--- concourse/create_iso.sh
+++ concourse/create_iso.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xu
+#!/bin/bash -u
 # Copyright 2017, 2018 RockNSM
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,21 +14,31 @@
 # limitations under the License.
 
 
-
+set x
 GPG_KEY_PATH="$(dirname "$(realpath "$0")")/rocknsm-2-sign.asc"
 GPG_KEY_NAME="$1"
+unset x
 GPG_KEY_PASS="$2"
 GPG_KEY="$3"
 
 # create the gpg key on disk to use for signing
 echo "$GPG_KEY" > "$GPG_KEY_PATH"
 # change working directory
+set x
 cd "$(dirname "$(realpath "$0")")"
 
 # Install dependencies
 . ../bootstrap.sh
 
 # Create ISO
+echo "passing the following variables to master-iso.sh"
+echo "-s ../../centos-minimal-iso/centos-minimal.iso"
+echo "-o rocknsm-$(date '+%Y%m%d').iso"
+echo "-g $GPG_KEY_NAME"
+echo "-p HIDDEN PASSWORD"
+echo "-i $GPG_KEY_PATH"
+
+unset x
 ../master-iso.sh \
 -s ../../centos-minimal-iso/centos-minimal.iso \
 -o "rocknsm-$(date '+%Y%m%d').iso" \
