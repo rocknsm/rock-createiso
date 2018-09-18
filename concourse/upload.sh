f9700be5cb13b31922af44ed2361f370368af104
Johnathon Hall <spartan782@gmail.com>
2018-09-18 12:09:20 -0600

    Update pulp admin conf

--- concourse/upload.sh
+++ concourse/upload.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+cat <<EOF | tee ~/.pulp/admin.conf
+[server]
+host = http://mirror.rocknsm.io
+verify_ssl = false
+EOF
+
 pulp-admin login -u $1 -p $2
 
 pulp-admin iso repo uploads upload --dir rocknsm-iso --repo-id rocknsm-nightly --bg
