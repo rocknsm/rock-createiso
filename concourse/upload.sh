e110adabcf021490c9f0012886028fa784238f7e
Johnathon Hall <spartan782@gmail.com>
2018-09-18 12:45:57 -0600

    make upload script executable

--- concourse/upload.sh
+++ concourse/upload.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
-
+#!/bin/bash -
+set -x
 cat <<EOF | tee ~/.pulp/admin.conf
 [server]
-host = http://mirror.rocknsm.io
+host = "$3"
 verify_ssl = false
 EOF
-
+echo "passing hidden values to pulp-admin login"
+set +x
 pulp-admin login -u $1 -p $2
-
+set -x
 pulp-admin iso repo uploads upload --dir rocknsm-iso --repo-id rocknsm-nightly --bg
