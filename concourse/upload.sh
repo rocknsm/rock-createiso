74b1c24923a4b0422298c2bedecca650240b972d
Johnathon Hall <spartan782@gmail.com>
2018-09-18 14:48:40 -0600

    Add pulp cert to upload

--- concourse/upload.sh
+++ concourse/upload.sh
@@ -1,12 +1,22 @@
 #!/bin/bash -
 set -x
+# Create certificate for pulp-admin
+cat <<EOF | tee ~/pulp.cert
+"$5"
+EOF
+
+# Create pulp admin configuration file
 cat <<EOF | tee ~/.pulp/admin.conf
 [server]
 host = "$3"
-verify_ssl = false
+verify_ssl = true
+ca_path = ~/pulp.cert
 EOF
+
+# Get auth token
 echo "passing hidden values to pulp-admin login"
 set +x
 pulp-admin login -u $1 -p $2
 set -x
-pulp-admin iso repo uploads upload --dir rocknsm-iso --repo-id rocknsm-nightly --bg
+# Upload iso file
+pulp-admin iso repo uploads upload --dir rocknsm-iso --repo-id $4
