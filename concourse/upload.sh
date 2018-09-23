4146576165161215421f96f48f0b2cbd46839e62
Johnathon Hall <spartan782@gmail.com>
2018-09-23 13:58:15 -0600

    Add repo publish commands
    Add publish commands so that the website syncs up with the uploaded iso and can be downloaded.

--- concourse/upload.sh
+++ concourse/upload.sh
@@ -1,21 +1,30 @@
 #!/bin/bash -
+PULP_USER=$1
+PULP_PASS=$2
+PULP_HOST=$3
+PULP_REPO=$4
+PULP_CERT=$5
+
+
 set -x
 # Create certificate for pulp-admin
 cat <<EOF | tee -a /etc/tls/certs/ca-bundle.crt
-$5
+$PULP_CERT
 EOF
 
 # Create pulp admin configuration file
 cat <<EOF | tee ~/.pulp/admin.conf
 [server]
-host = $3
+host = $PULP_HOST
 verify_ssl = true
 EOF
 
 # Get auth token
 echo "passing hidden values to pulp-admin login"
 set +x
-pulp-admin login -u $1 -p $2
+pulp-admin login -u $PULP_USER -p $PULP_PASS
 set -x
 # Upload iso file
-pulp-admin iso repo uploads upload --dir rocknsm-iso --repo-id $4
+pulp-admin iso repo uploads upload --dir rocknsm-iso --repo-id $PULP_REPO
+
+pulp-admin iso repo publish run --repo-id $PULP_REPO
