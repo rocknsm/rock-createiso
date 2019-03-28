2a8a520d3f677af6173b0be7ab72baedb46a9596
Johnathon Hall <spartan782@gmail.com>
2019-03-28 12:18:49 -0600

    Change concourse iso upload dir

--- concourse/upload.sh
+++ concourse/upload.sh
@@ -16,6 +16,6 @@ echo "$LOCAL_MIRROR_PUBLIC" > "/root/.ssh/id_ed25519.pub"
 # change the permissions on the keys
 chmod 0600 /root/.ssh/*
 # Upload to local mirror
-rsync -e "ssh -o StrictHostKeyChecking=no -i /root/.ssh/id_ed25519" -rv rocknsm-iso/ $LOCAL_MIRROR_USER@$LOCAL_MIRROR_HOST:/var/www/mirror/isos/$LOCAL_MIRROR_REPO/
+rsync -e "ssh -o StrictHostKeyChecking=no -i /root/.ssh/id_ed25519" -rv rocknsm-iso/ $LOCAL_MIRROR_USER@$LOCAL_MIRROR_HOST:/var/www/mirror/public/isos/$LOCAL_MIRROR_REPO/
 # Sync to public mirror
-ssh -i /root/.ssh/id_ed25519 -o StrictHostKeyChecking=no admin@mirror.cyberlab.lan 'rsync -rlvtP -e "ssh -i ~/.ssh/mirror_sync" /var/www/mirror/ mirror_sync@mirror.rocknsm.io:/var/www/mirror/ --delete' 
+ssh -i /root/.ssh/id_ed25519 -o StrictHostKeyChecking=no admin@mirror.cyberlab.lan 'rsync -rlvtP -e "ssh -i ~/.ssh/mirror_sync" /var/www/mirror/ mirror_sync@mirror.rocknsm.io:/var/www/mirror/ --delete'
