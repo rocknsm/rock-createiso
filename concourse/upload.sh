b80ef10f570215f4c7202856e1d66b5d9899d9c3
Derek Ditch <dcode@users.noreply.github.com>
2019-03-28 13:43:51 -0500

    Adjusted the local to remote mirror sync too

--- concourse/upload.sh
+++ concourse/upload.sh
@@ -18,4 +18,4 @@ chmod 0600 /root/.ssh/*
 # Upload to local mirror
 rsync -e "ssh -o StrictHostKeyChecking=no -i /root/.ssh/id_ed25519" -rv rocknsm-iso/ $LOCAL_MIRROR_USER@$LOCAL_MIRROR_HOST:/var/www/mirror/public/isos/$LOCAL_MIRROR_REPO/
 # Sync to public mirror
-ssh -i /root/.ssh/id_ed25519 -o StrictHostKeyChecking=no admin@mirror.cyberlab.lan 'rsync -rlvtP -e "ssh -i ~/.ssh/mirror_sync" /var/www/mirror/ mirror_sync@mirror.rocknsm.io:/var/www/mirror/ --delete'
+ssh -i /root/.ssh/id_ed25519 -o StrictHostKeyChecking=no admin@mirror.cyberlab.lan 'rsync -rlvtP -e "ssh -i ~/.ssh/mirror_sync" /var/www/mirror/public/ mirror_sync@mirror.rocknsm.io:/var/www/mirror/ --delete'
