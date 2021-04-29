7aeac32b0a4684a3483152828b4ba4241a08f063
Keoni Byron <kbyron11@gmail.com>
2021-04-29 14:08:06 -0700

    Update upload.sh
    update perms on .ssh folder

--- concourse/upload.sh
+++ concourse/upload.sh
@@ -13,7 +13,9 @@ mkdir -p /root/.ssh
 echo "$LOCAL_MIRROR_PRIVATE" > "/root/.ssh/id_ed25519"
 set -x
 echo "$LOCAL_MIRROR_PUBLIC" > "/root/.ssh/id_ed25519.pub"
-# change the permissions on the keys
+# change the permissions on the .ssh folder
+chmod -R 0700 /root/.ssh/
+#change permission on the keys
 chmod 0600 /root/.ssh/*
 # Upload to local mirror
 rsync -e "ssh -o StrictHostKeyChecking=no -i /root/.ssh/id_ed25519" -rv rocknsm-iso/ $LOCAL_MIRROR_USER@$LOCAL_MIRROR_HOST:/var/www/mirror/public/isos/$LOCAL_MIRROR_REPO/
