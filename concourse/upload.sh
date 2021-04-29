10c3c290651e2b49dbcef3ad4e0089fcb37f5224
Keoni Byron <kbyron11@gmail.com>
2021-04-29 15:21:24 -0700

    Update upload.sh
    remove the port from rsync command

--- concourse/upload.sh
+++ concourse/upload.sh
@@ -5,6 +5,7 @@ LOCAL_MIRROR_HOST=$3
 LOCAL_MIRROR_REPO=$4
 LOCAL_MIRROR_PRIVATE=$5
 LOCAL_MIRROR_PUBLIC=$6
+LOCAL_MIRROR_IP=$7
 
 yum install rsync openssh-clients -y
 echo "exporting hidded keys for password less sync"
@@ -18,6 +19,6 @@ chmod -R 0700 /root/.ssh/
 #change permission on the keys
 chmod 0600 /root/.ssh/*
 # Upload to local mirror
-rsync -e "ssh -o StrictHostKeyChecking=no -i /root/.ssh/id_ed25519" -rv rocknsm-iso/ $LOCAL_MIRROR_USER@$LOCAL_MIRROR_HOST:/var/www/mirror/public/isos/$LOCAL_MIRROR_REPO/
+rsync -e "ssh -o StrictHostKeyChecking=no -i /root/.ssh/id_ed25519" -rv rocknsm-iso/ $LOCAL_MIRROR_USER@$LOCAL_MIRROR_IP:/var/www/mirror/public/isos/$LOCAL_MIRROR_REPO/
 # Sync to public mirror
 ssh -i /root/.ssh/id_ed25519 -o StrictHostKeyChecking=no admin@${LOCAL_MIRROR_HOST} 'rsync -rlvtP -e "ssh -i ~/.ssh/mirror_sync" /var/www/mirror/public/ mirror_sync@mirror.rocknsm.io:/var/www/mirror/ --delete'
